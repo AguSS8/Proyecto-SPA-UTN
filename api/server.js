b830625ebc5d109dbc0246c7ac74c61b4d44ee26
AguSS8 <agustinnicosalto@gmail.com>
2022-09-29 21:11:44 -0300

    variables de entorno
    incorporacion de las env al proyecto

--- api/server.js
+++ api/server.js
@@ -1,13 +1,19 @@
 const express = require ('express');
 const router = require('./routes/router');
+const dotenv = require('dotenv');
+
 
 const app = express();
 
 app.use(express.urlencoded({extended:true}));
 app.use(express.json());
 
+dotenv.config({path:'./env/.env'})
+
+app.set('port', process.env.PORT ||3001);
+
 app.use('/', router);
 
-app.listen(3002, ()=>{
-  console.log('Server on port ' + 3002);
+app.listen(app.get('port'), ()=>{
+  console.log('Server on port ' + app.get('port'));
 })
