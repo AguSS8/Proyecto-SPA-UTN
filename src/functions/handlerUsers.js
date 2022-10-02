40d9161357d91ecfbe0e5f7742da1404dbc6155c
AguSS8 <agustinnicosalto@gmail.com>
2022-10-02 14:20:48 -0300

    login FE
    Se mandan los datos del login del frontend a la api

--- src/functions/handlerUsers.js
+++ src/functions/handlerUsers.js
@@ -14,3 +14,16 @@ export const register = (e)=>{
     });
 }
 
+export const login = e => {
+  e.preventDefault();
+  let mail = document.getElementById("mail").value;
+  let pass = document.getElementById("pass").value;
+  const datos = {mail, pass};
+    fetch('http://localhost:3000/login', {
+      method: 'POST',
+      headers: {
+        "Content-type": "application/json"
+      },
+      body: JSON.stringify(datos)
+    });
+}
